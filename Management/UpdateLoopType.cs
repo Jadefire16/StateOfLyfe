d8be2610a839b8d14eb5b39a8a2a9ac6bbf83236
Zach <53870574+Jadefire16@users.noreply.github.com>
2023-09-03 02:21:42 -0400

    index on main: 95bfc27 Initial Commit

---
+++ Management/UpdateLoopType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOL
+{
+    [Flags]
+    enum UpdateLoopType
+    {
+        Invalid = 0,
+        EarlyTick = 1,
+        Tick = 2,
+        FixedTick = 4,
+        LateTick = 8,
+    }
+}
